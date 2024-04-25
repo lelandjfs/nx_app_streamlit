06bcb2c3ea7f7b953a925c8fcfd46e407d13b181
lelandjfs <121068868+lelandjfs@users.noreply.github.com>
2024-04-25 01:21:32 +0000

    Day 2.1

--- nxapp.py
+++ nxapp.py
@@ -3,8 +3,10 @@ import streamlit as st
 st.title("AI Developer Network Map")
 
 
+summary_text = "The purpose of this app is to generate a network graph of an inputted Linkedin connections download CSV."
+
 # Display the summary field
-summary_placeholder = st.empty()
+summary_placeholder = st.header(text)
 
 # Create a file uploader widget
 uploaded_file = st.file_uploader("Choose a file")
